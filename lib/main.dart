9baa4c9b5d51481489163ac8266fa0fc2042aac0
Rounak Agrawal <rounak1430@gmail.com>
2023-04-11 11:17:03 +0530

    Main Program written and theme initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Weather App",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryTextTheme: Typography().white,
+            scaffoldBackgroundColor: const Color(0xFF0B0C1E),
+            brightness: Brightness.dark),
+        initialRoute: "/",
+        routes: {});
+  }
+}
