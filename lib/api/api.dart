2073158b9119e3e3c4caf61dc32297f678d50efa
Rounak Agrawal <rounak1430@gmail.com>
2023-04-11 21:51:47 +0530

    Weather Model created and api configured

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -2,8 +2,6 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app/env.dart';
 
 const String baseUrl = "api.openweathermap.org";
-const String locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
-const String weatherUrl = "http://api.openweathermap.org/data/2.5/weather?";
 const String appId = ENV.appId;
 
 class Api {
@@ -24,7 +22,7 @@ class Api {
     Uri url = Uri(
         scheme: "https",
         host: baseUrl,
-        path: "geo/1.0/direct",
+        path: "data/2.5/weather",
         queryParameters: {
           "lat": lat,
           "lon": lon,
